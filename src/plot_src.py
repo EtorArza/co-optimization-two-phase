ee49e6a11fde73bbbdfd3ede0bb369f22acdc261
EtorArza <etorarza@gmail.com>
2023-02-16 13:55:55 +0100

    plot evogym working

--- src/plot_src.py
+++ src/plot_src.py
@@ -4,6 +4,10 @@ import os
 from tqdm import tqdm as tqdm
 import numpy as np
 
+marker_list = ["","o"]
+linestyle_list = ["-","--"]
+color_list = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+
 def bootstrap_median_and_confiance_interval(data,bootstrap_iterations=1000):
     mean_list=[]
     for i in range(bootstrap_iterations):
@@ -13,6 +17,7 @@ def bootstrap_median_and_confiance_interval(data,bootstrap_iterations=1000):
 
 
 def read_comparison_parameter_csvs(csv_folder_path):
+    print("Reading Dataframes...")
     df = pd.DataFrame(columns=["level","f_best","f","time","step","iteration","evaluation","env_name","max_steps","inner_quantity","inner_length","seed"])
     for csv_name in tqdm(os.listdir(csv_folder_path)):
         if ".txt" in csv_name:
@@ -31,55 +36,48 @@ def plot_comparison_parameters(csv_folder_path, figpath, exp_name):
     max_steps = max(df["step"])
     n_seeds = len(df["seed"].unique())
 
-    # df.iloc[df[df["steps"] < 450].groupby("seed")["score"].idxmax()]
 
     # # Check how many steps where computed.
     # plt.boxplot(df.groupby(["seed", "inner_quantity", "inner_length"])["step"].max())
     # plt.show()
     
-
-
-    print(df.dtypes)
-    # import code; code.interact(local=locals()) # start interactive shell for debugging
-
+    # # Print all seeds with certain parameters.
     # df = df[df["level"]==3]
     # df = df[df["inner_quantity"]=='0.5']
     # df = df[df["inner_length"]=='0.5']
-    # for seed in range(5,19):
+    # for seed in range(2,19):
     #     # print(df[df["seed"]==seed])
     #     # print(df.query(f"seed == {seed}"))
-    #     plt.plot(df.query(f"seed == {seed}")["step"], df[(df["seed"]==seed)]["f"])
+    #     plt.plot(df.query(f"seed == {seed}")["step"], df[(df["seed"]==seed)]["f"], label=seed)
+    # plt.legend()
     # plt.show()
-    # df.reset_index()
-    # print(df)
     # exit(1)
     
+    # Get the parameter values such that 1.0 is the first
+    inner_quantity_values = sorted(list(df["inner_quantity"].unique()), key=lambda x: -4*float(x) + 2*float(x)*float(x))
+    inner_length_values = sorted(list(df["inner_length"].unique()), key=lambda x: -4*float(x) + 2*float(x)*float(x))
 
+
+
+    print("Generating plot...")
+    step_slices = 30
     i=-1
-    for group_name, df_group in df.groupby(["inner_quantity", "inner_length"]):
+    for group_name, df_group in tqdm(df.groupby(["inner_quantity", "inner_length"])):
         i+=1
+
         x = []
         y_mean = []
         y_lower = []
         y_upper = []
 
-        marker = {
-            "0.5":"o",
-            "1.0":"",    
-        }[group_name[0]] # inner_quantity
+        marker = marker_list[inner_quantity_values.index(group_name[0])]
+        linestyle = linestyle_list[inner_quantity_values.index(group_name[1])]
+        color = color_list[i]
 
-        linestyle = {
-            "0.5":"--",
-            "1.0":"-",    
-        }[group_name[1]] # inner_length
         df_group = df_group.reset_index()
 
 
-        color = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'][i]
 
-        print(marker, linestyle, color)
-
-        step_slices = 50
         for step in np.linspace(0, max_steps, step_slices):
             selected_indices = df_group[df_group["step"] < step].groupby("seed")["f"].idxmax()
             scores = np.array(df_group.iloc[selected_indices]["f"])
@@ -94,8 +92,12 @@ def plot_comparison_parameters(csv_folder_path, figpath, exp_name):
         plt.plot(x, y_mean, color=color, linestyle=linestyle, marker=marker, markevery=1/5)
         plt.fill_between(x, y_lower, y_upper, alpha=0.1, color=color, linestyle=linestyle)
 
-    plt.plot([],[],label="inner_quantity = 0.5", marker="o", color="black")
-    plt.plot([],[],label="inner_length = 0.5", linestyle="--",color="black")
+    for inner_quantity, marker in zip(inner_quantity_values[1:], marker_list[1:]):
+        plt.plot([],[],label=f"inner_quantity = {inner_quantity}", marker="o", color="black")
+
+    for inner_length, linestyle in zip(inner_length_values[1:], linestyle_list[1:]):
+
+        plt.plot([],[],label=f"inner_length = {inner_length}", linestyle="--",color="black")
     plt.legend()
     plt.savefig(figpath + "/comparison_cutting_controller_budget.pdf")
     plt.ylim((10,11))
