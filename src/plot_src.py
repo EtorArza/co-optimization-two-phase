9efec1ad54f67bd54fbd73fb6992cf7a35282e57
EtorArza <etorarza@gmail.com>
2023-02-28 11:49:10 +0100

    adapt plot_src to new savefig

--- src/plot_src.py
+++ src/plot_src.py
@@ -4,8 +4,8 @@ import os
 from tqdm import tqdm as tqdm
 import numpy as np
 
-marker_list = ["","o"]
-linestyle_list = ["-","--"]
+marker_list = ["","o","x"]
+linestyle_list = ["-","--","-."]
 color_list = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
 def bootstrap_median_and_confiance_interval(data,bootstrap_iterations=1000):
@@ -18,11 +18,35 @@ def bootstrap_median_and_confiance_interval(data,bootstrap_iterations=1000):
 
 def read_comparison_parameter_csvs(csv_folder_path):
     print("Reading Dataframes...")
-    df = pd.DataFrame(columns=["level","f_best","f","time","step","iteration","evaluation","env_name","max_steps","inner_quantity","inner_length","seed"])
+    dtypes={
+        "level":object,
+        "evaluation": np.int64,
+        "f_best": np.float64,
+        "f": np.float64,
+        "controller_size": np.float64,
+        "controller_size2": np.float64,
+        "morphology_size": np.float64,
+        "time": np.float64,
+        "time_including_reeval": np.float64,
+        "step": np.int64,
+        "step_including_reeval": np.int64,
+        "experiment_index": object,
+        "env_name": object,
+        "max_steps": int,
+        "inner_quantity": object,
+        "inner_length": object,
+        "seed": int,
+    }
+
+
+    df = pd.DataFrame(columns=["level","evaluation","f_best","f","controller_size","controller_size2","morphology_size","time","time_including_reeval","step","step_including_reeval","experiment_index","env_name","max_steps","inner_quantity","inner_length","seed"])
+    df = df.astype(dtype=dtypes)
     for csv_name in tqdm(os.listdir(csv_folder_path)):
         if ".txt" in csv_name:
-            n_df = pd.read_csv(csv_folder_path+"/"+csv_name)
-            env_name,max_steps,inner_quantity, inner_length, seed = csv_name.removesuffix(".txt").split("_")
+
+            n_df = pd.read_csv(csv_folder_path+"/"+csv_name, header=0, dtype=dtypes)
+            experiment_index,env_name,max_steps,inner_quantity, inner_length, seed = csv_name.removesuffix(".txt").split("_")
+            n_df["experiment_index"] = experiment_index
             n_df["env_name"] = env_name
             n_df["max_steps"] = int(max_steps)
             n_df["inner_quantity"] = inner_quantity
@@ -37,10 +61,18 @@ def plot_comparison_parameters(csv_folder_path, figpath, exp_name):
     n_seeds = len(df["seed"].unique())
 
 
-    # # Check how many steps where computed.
-    # plt.boxplot(df.groupby(["seed", "inner_quantity", "inner_length"])["step"].max())
-    # plt.show()
+    # Check how many steps where computed.
+    indices_with_highest_step = np.array(df.groupby(by="experiment_index")["step"].idxmax())
+    max_only_df = df.loc[indices_with_highest_step,]
+
+    ax = max_only_df.boxplot(by=["inner_quantity", "inner_length"], column="step")
+
+    ax.set_title("")
+    ax.set_ylabel("step")
+    plt.savefig(figpath + "/number_of_steps.pdf")
+    plt.close()
     
+
     # # Print all seeds with certain parameters.
     # df = df[df["level"]==3]
     # df = df[df["inner_quantity"]=='0.5']
@@ -93,11 +125,11 @@ def plot_comparison_parameters(csv_folder_path, figpath, exp_name):
         plt.fill_between(x, y_lower, y_upper, alpha=0.1, color=color, linestyle=linestyle)
 
     for inner_quantity, marker in zip(inner_quantity_values[1:], marker_list[1:]):
-        plt.plot([],[],label=f"inner_quantity = {inner_quantity}", marker="o", color="black")
+        plt.plot([],[],label=f"inner_quantity = {inner_quantity}", marker=marker, color="black")
 
     for inner_length, linestyle in zip(inner_length_values[1:], linestyle_list[1:]):
 
-        plt.plot([],[],label=f"inner_length = {inner_length}", linestyle="--",color="black")
+        plt.plot([],[],label=f"inner_length = {inner_length}", linestyle=linestyle,color="black")
     plt.legend()
     plt.savefig(figpath + "/comparison_cutting_controller_budget.pdf")
     plt.ylim((10,11))
