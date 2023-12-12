7298002dd2695a847854a4fb914437b55f3a787d
Etor Arza <etorarza@gmail.com>
2023-12-12 19:08:29 +0100

    add script to update all  plots in paper

--- all_plots_background.sh
+++ all_plots_background.sh
@@ -1,3 +1,19 @@
-nohup python src/evogym_experiment.py --plot &
-nohup python src/robogrammar_experiment.py --plot &
-nohup python src/gymrem2d_experiment.py --plot &
+source venv/bin/activate
+
+python src/jorgenrem_experiment.py --plot &
+python src/evogym_experiment.py --plot &
+python src/robogrammar_experiment.py --plot &
+python src/gymrem2d_experiment.py --plot &
+
+
+wait
+echo "produced all the plots"
+
+# Copy results to paper dir
+rsync -zarv --delete --prune-empty-dirs --include '*/' --include '*.pdf' --exclude '*' results/ ../paper/results/
+
+# Recompile paper
+current=`pwd`
+cd ../paper
+pdflatex -synctex=1 -interaction=nonstopmode main.tex
+cd $current 
